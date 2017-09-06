04ca95bc3fdbe64ebe89d60088c47cb608cbafd7
Mario <mario.saliba98@gmail.com>
2017-09-07 01:03:59 +0300

    routes

---
+++ src/router.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import App from './containers/app.jsx';
+import MethodList from './containers/method_list.jsx';
+import Directions from './containers/directions.jsx';
+
+const Routes = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path='/' exact component={App} />
+        <Route path='/place/:guesthouse' component={MethodList} />
+        <Route path='/car' exact component={Directions} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
+export default Routes;
