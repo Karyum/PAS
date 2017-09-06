6ee15fd32978545b96b1e64bf244ba09475c0b17
Mario <mario.saliba98@gmail.com>
2017-09-07 00:44:47 +0300

    adds action for reducers

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,17 @@
+import { CHOSE_GUESTHOUSE, CAR_METHOD } from '../constants/action_types.js';
 
+export const setGuesthouse = (guesthouse) => {
+  return {
+    type : CHOSE_GUESTHOUSE,
+    guesthouse: {
+      title: guesthouse.title
+    }
+  }
+}
+
+export const setMethod = (method) => {
+  return {
+    type: CAR_METHOD,
+    method
+  }
+}
