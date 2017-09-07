8e1daa374539eba1eb2d5b3955209a27d35df664
Mario <mario.saliba98@gmail.com>
2017-09-07 12:31:52 +0300

    gets the cords from the server with axios

--- src/containers/directions.jsx
+++ src/containers/directions.jsx
@@ -1,28 +1,63 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Axios from 'axios';
+import ReactMapboxGl from 'react-mapbox-gl';
 
 import { setMethod } from '../actions/index.js';
-import { CAR_METHOD } from '../constants/action_types.js';
+import { CAR_METHOD, BUS_METHOD, TAXI_METHOD } from '../constants/action_types.js';
+
 
 class Directions extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {latlng: {}}
+  }
+
   componentWillMount() {
-    this.props.setMethod(CAR_METHOD);
+    const { path } = this.props.match
+    this.props.setMethod(this.findMethod(path));
+
+    Axios.post('/geocode', {
+      address: this.props.current.address
+    }).then(res => {
+       console.log(res.data);
+       this.setState({ latlng: res.data })
+    }).catch(err => {
+      console.log(err);
+      return err
+    })
+
+  }
+
+   findMethod(method) {
+    switch (method) {
+      case '/car':
+        return CAR_METHOD
+        break;
+      case '/bus':
+        return BUS_METHOD
+        break;
+      case '/taxi':
+        return TAXI_METHOD
+      default:
+        return method
+    }
   }
 
   render() {
     return (
       <div>
-        <h1>{this.props.current}</h1>
         <h2>{this.props.currentMethod}</h2>
+        <h1>{this.props.current.title}</h1>
       </div>
     )
   }
 
 }
 const mapStateToProps = (state) => {
-  console.log(state);
   return {
-    current: state.current.title,
+    current: state.current,
     currentMethod: state.currentMethod
   }
 }
