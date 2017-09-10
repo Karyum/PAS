970e7db8005966bad7bf142eaf14ae18b94ebbbf
Mario <mario.saliba98@gmail.com>
2017-09-10 23:02:21 +0300

    call the user's location

--- src/containers/app.jsx
+++ src/containers/app.jsx
@@ -2,20 +2,34 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import GuesthouseList from '../components/guesthouse_list.jsx';
-import { setGuesthouse } from '../actions/index.js';
+import { setGuesthouse, setLocation } from '../actions/index.js';
 
-const App = ({ guesthouses, reducer }) => {
+class App extends Component {
+  constructor(props) {
+    super();
+
+    this.state = {}
+  }
+
+  componentWillMount() {
+    navigator.geolocation.getCurrentPosition((postion) => {
+      return this.props.setLocation(postion.coords)
+    })
+  }
+  render() {
     return (
-        <GuesthouseList guesthouses={guesthouses} reducer={reducer} />
+        <GuesthouseList guesthouses={this.props.guesthouses} reducer={this.props.setGuesthouse} />
     )
+  }
 }
 
   const mapStateToProps = (state) => ({
   guesthouses: state.guesthouses
 })
 
-const mapDispatchToProps = (dispatch) => ({
-  reducer: (guesthouse) =>  dispatch(setGuesthouse(guesthouse))
-})
+const mapDispatchToProps = {
+  setGuesthouse,
+  setLocation
+}
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
