f8df96046b0f228ac92f0c09998b944f5f3adfe1
Mario <mario.saliba98@gmail.com>
2017-09-10 23:01:27 +0300

    adds the reducer to the state object

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,11 +3,13 @@ import { combineReducers } from 'redux';
 import GuesthouseReducer from './guesthouse.reducer.js';
 import CurrentGuestHouse from './current.reducer.js';
 import CurrentMethod from './currentMethod.reducer.js';
+import LocationReducer from './location.reducer.js';
 
 const rootReducers = combineReducers({
   guesthouses: GuesthouseReducer,
   current: CurrentGuestHouse,
-  currentMethod: CurrentMethod
+  currentMethod: CurrentMethod,
+  usersLocation: LocationReducer
 })
 
 export default rootReducers;
