818c1419ae5984fa980d31b6b9c45319d8f4e812
Mario <mario.saliba98@gmail.com>
2017-09-13 19:54:57 +0300

    implement leaflet map

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -1,35 +1,34 @@
 import React, { Component } from 'react';
-import ReactMapboxGl, { Marker, Popup, Layer, Feature } from 'react-mapbox-gl';
-import mapbox from 'mapbox-gl';
+import { Redirect } from 'react-router';
 import Icon from '../../public/assets/circle.png';
+import 'leaflet/dist/leaflet.css';
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
 
-const MapBox = ReactMapboxGl({
-  accessToken: 'pk.eyJ1Ijoia2FyeXVtIiwiYSI6ImNqMjAzNGU4ZjAxa3EycW4xazFxcHZ6a2QifQ.m_dNO1l1sMkM7r4d5nlRRQ',
-  maxZoom: 50
-})
-const Map = (props) => {
+class MapView extends Component  {
+  constructor(props) {
+    super();
+
+    this.state = {}
+  }
+  render() {
+    if (!this.props.coords.usersLat) {
+      render() {
+        return (
+
+        )
+      }
+    }
   return (
-    <div>
-      <MapBox
-        containerStyle={{
-          height: "75vh",
-          width: "100vw"
-        }}
-        center={[props.coords.usersLong, props.coords.usersLat]}
-        style="mapbox://styles/mapbox/streets-v10">
-
-        <Marker
-          coordinates={[props.coords.usersLong, props.coords.usersLat]}>
-          <img src={Icon} />
-        </Marker>
-
-        <Layer
-          type="line">
-          <Feature coordinates={[[35.297201799999996, 32.7017354], [35.2979486, 32.7043734]]}></Feature>
-        </Layer>
-      </MapBox>
-    </div>
+    <Map style={{
+        height: "75vh",
+        width: "100vw"
+      }} center={[this.props.coords.usersLat, this.props.coords.usersLong]} zoom={10}>
+      <TileLayer
+        url='https://api.mapbox.com/styles/v1/mapbox/navigation-preview-day-v2/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoia2FyeXVtIiwiYSI6ImNqMjAzNGU4ZjAxa3EycW4xazFxcHZ6a2QifQ.m_dNO1l1sMkM7r4d5nlRRQ'
+      />
+    </Map>
   )
 }
+}
 
-export default Map;
+export default MapView;
