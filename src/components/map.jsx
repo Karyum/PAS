c7d1bb5b52f5ff57bca93f7c33459407610b3d08
Mario <mario.saliba98@gmail.com>
2017-09-17 22:24:25 +0300

    adds a marker to the map of the users location

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import { Redirect } from 'react-router';
+import { Redirect } from 'react-router-dom';
 import Icon from '../../public/assets/circle.png';
-import 'leaflet/dist/leaflet.css';
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
 
 const MapView = (props) => {
@@ -14,8 +13,14 @@ const MapView = (props) => {
         }}
         center={[props.coords.usersLat, props.coords.usersLong]} zoom={15}>
         <TileLayer
-          url='https://api.mapbox.com/styles/v1/mapbox/navigation-preview-day-v2/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoia2FyeXVtIiwiYSI6ImNqMjAzNGU4ZjAxa3EycW4xazFxcHZ6a2QifQ.m_dNO1l1sMkM7r4d5nlRRQ'
+          url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
         />
+      <Marker
+        position={[props.coords.usersLat, props.coords.usersLong]}
+        >
+      </Marker>
+
+      
       </Map>
     )
   } else {
