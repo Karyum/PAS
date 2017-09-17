41491c98d5c41185f0a4c7753d19d832e6af6200
Mario <mario.saliba98@gmail.com>
2017-09-17 22:21:52 +0300

    remove car and bus endpoints

--- src/router.jsx
+++ src/router.jsx
@@ -3,7 +3,8 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import App from './containers/app.jsx';
 import MethodList from './containers/method_list.jsx';
-import Directions from './containers/directions.jsx';
+import Walking from './containers/walking.jsx';
+import GoogleMethod from './containers/google_method.jsx';
 
 const Routes = () => {
   return (
@@ -11,9 +12,9 @@ const Routes = () => {
       <Switch>
         <Route path='/' exact component={App} />
         <Route path='/place/:guesthouse' component={MethodList} />
-        <Route path='/car' exact component={Directions} />
-        <Route path='/bus' exact component={Directions} />
-        <Route path='/taxi' exact component={Directions} />
+        <Route path='/car' exact component={GoogleMethod} />
+        <Route path='/bus' exact component={GoogleMethod} />
+        <Route path='/walking' exact component={Walking} />
       </Switch>
     </BrowserRouter>
   );
