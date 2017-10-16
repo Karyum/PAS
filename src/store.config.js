bfd65200deb8f664198c0185c682d172dc7cd38a
Mario <mario.saliba98@gmail.com>
2017-10-16 18:29:47 +0300

    setup thunk as redux middleware

--- src/store.config.js
+++ src/store.config.js
@@ -1,25 +1,10 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 // import persistState from 'redux-localstorage';
+import ReduxThunk from 'redux-thunk';
 import reducers from './reducers';
 
 const storeConfigure = () => {
-  const initialStore = {
-    allGuesthouses: [
-      { title: 'Liwan', address: '6132 St 22-24, Nazareth', latitude: 32.704519, longtitude: 35.298089 },
-      { title: 'Fauzi Azar', address: '6108 St, Nazareth, 16125', latitude: 32.704827, longtitude: 35.297459 },
-      { title: 'AlReda', address: 'Al-Bishara St 21, Nazareth', latitude: 32.703154, longtitude: 35.297981 }
-    ],
-    selectedGuesthouse: {},
-    directions: {}
-  };
-
-  // store it in local storage
-  // const enhancer = compose(
-  //   persistState()
-  // )
-
-  const store = createStore(reducers, initialStore);
-
+  const store = createStore(reducers, applyMiddleware(ReduxThunk));
   return store;
 };
 
